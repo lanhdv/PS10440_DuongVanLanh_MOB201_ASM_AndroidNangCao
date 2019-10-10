0cd9110a51702b946b1a4a16d15eba650d43ddde
lanhdv1403 <lanhdv1403@gmail.com>
2019-10-10 11:57:24 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ps10440_duongvanlanh_mob201_asm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ps10440_duongvanlanh_mob201_asm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
